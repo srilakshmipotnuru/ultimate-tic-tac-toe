fec944625cc5c60f15904f4a069ccb40054fd234
srilakshmipotnuru <84777615+srilakshmipotnuru@users.noreply.github.com>
2025-07-14 20:27:00 +0530

    pixel font setup and other style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Home } from 'lucide-react'
 import Header from './components/Header'
+import HomePageBody from './components/HomePageBody'
 
 function App() {
 
   return (
-    <div className='bg-[var(--background)] h-[100vh]'>
+    <div className='bg-[var(--background-100)] h-[100vh]'>
       <Header/>
+      <HomePageBody/>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./ThemeToggle"
 const Header = () => {
     return (
         <div className="flex items-center justify-between px-6 py-3 bg-[var(--background-300)] h-[10vh]">
-            <h1 className="text-xl font-bold tracking-wide text-[var(--text)]">Tic-Tac-Toe</h1>
+            <h1 className="text-2xl pixel-bold tracking-wide text-[var(--text)]">Tic-Tac-Toe</h1>
             <ThemeToggle/>
         </div>
     )
