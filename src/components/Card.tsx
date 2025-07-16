21fffb12f32b65b5957034e1f52c20d98a8217f7
srilakshmipotnuru <84777615+srilakshmipotnuru@users.noreply.github.com>
2025-07-16 10:23:14 +0530

    designed cards for all modes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,36 @@
-
+import { ChevronRightIcon, ChevronDownIcon } from "raster-react"
+import { useState } from "react"
 
 interface CardProps {
     title: string
     description: string
+    modes?: string[]
 }
 
-const Card = (props: CardProps) => {
-    const {title, description} = props
+const Card = ({ title, description, modes }: CardProps) => {
+    const [showDesc, setShowDesc] = useState(false)
+
     return (
-        <div className="shadow-[8px_8px_0_0_var(--accent)] p-4 bg-[var(--background-300)] rounded-none hover:shadow-[8px_8px_0_0_var(--accent-400)] transition-shadow duration-200">
-            {title}
-            {description}
+        <div
+            className="flex flex-col gap-4 shadow-[8px_8px_0_0_var(--accent-400)] p-4 bg-[var(--background-300)] rounded-none border-4 border-[var(--secondary-500)] transition-shadow duration-200"
+        >
+            <h2 className="pixel-bold text-xl text-[var(--accent-600)] ">{title}</h2>
+            <button onClick={() => setShowDesc(!showDesc)} className="flex items-center gap-2" >
+                {showDesc ? <ChevronDownIcon size={50} color="var(--accent-600)" strokeWidth={0.25} radius={1} /> : <ChevronRightIcon size={50} color="var(--accent-600)" strokeWidth={0.25} radius={1} />}
+                <p>{showDesc ? `Hide description` : `Show description`}</p>
+            </button>
+            {showDesc && <div className="ml-15 text-sm sm:text-base">{description}</div>}
+            {modes ? (
+                <div className="flex flex-wrap gap-2 mt-2 justify-center sm:justify-start">
+                    {modes.map((mode, index) => (
+                        <button key={index} className="px-3 py-1 shadow-[4px_4px_0_0_var(--accent-400)] bg-[var(--background-200)] border border-[var(--secondary-500)]">
+                            Play {mode}
+                        </button>
+                    ))}
+                </div>
+            ) : <button className="mt-2 px-3 w-auto sm:w-max py-1 shadow-[4px_4px_0_0_var(--accent-400)] bg-[var(--background-200)] border border-[var(--secondary-500)]">
+                Play {title}
+            </button>}
         </div>
     )
 }
--- src/components/HomePageBody.tsx
+++ src/components/HomePageBody.tsx
@@ -1,16 +1,16 @@
 import Card from "./Card"
 
-const classicDescription = "This is the classic version of the game. you can choose out of 3 grids 3*3, 5*5, and 7*7."
+const classicDescription = "This is the classic version of the game. you can choose out of 3 grids 3x3, 5x5, and 7x7."
 const superDescription = "This advanced version features 9 mini-boards within a 3x3 grid. The next board a player must play on is determined by the position of the previous move. Win any one of the mini-boards to win."
 const ultimateDescription = "This is the ultimate version of the game. Like in the advanced version, there are 9 mini-boards arranged in a 3x3 grid. However, to win, a player must claim a full row, column, or diagonal (the usual winning conditions) on the larger grid by winning the corresponding mini-board."
 
 const HomePageBody = () => {
     return (
-        <div className="flex-col gap-y-6 home-page-body p-2 sm:p-4 text-[var(--text)]">
+        <div className="flex-col gap-y-6 home-page-body p-6 sm:p-4 text-[var(--text)]">
             <h1 className="mb-4">Welcome to the Tic Tac Toe Game!</h1>
             <p className="mb-6">Choose your mode, grab a friend, and get ready to play!</p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 bg-[var(--background-200)] rounded-none">
-                <Card title="Classic Tic Tac Toe" description={classicDescription} />
+            <div className="flex flex-col gap-4 p-4 bg-[var(--background-200)] rounded-none">
+                <Card title="Classic Tic Tac Toe" description={classicDescription} modes={['3x3', '5x5', '7x7']} />
                 <Card title="Super Tic Tac Toe" description={superDescription} />
                 <Card title="Ultimate Tic Tac Toe" description={ultimateDescription} />
             </div>
